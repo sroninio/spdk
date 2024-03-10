c662853200b79091a4aa87825d571f28af09d873
Shuhei Matsumoto <smatsumoto@nvidia.com>
2024-03-10 11:16:13 +0000

    bdev: Factor out per bdev_qos_limit operations into helper functions
    This improves code readability and makes us easier to do further changes.
    
    Signed-off-by: Shuhei Matsumoto <smatsumoto@nvidia.com>

--- lib/bdev/bdev_qos_limits.c
+++ lib/bdev/bdev_qos_limits.c
@@ -191,120 +191,149 @@ bdev_qos_limit_w_bps_rewind_quota(struct bdev_qos_limit *limit,
 }
 
 static void
+bdev_qos_limit_set_ops(struct bdev_qos_limit *limit, enum spdk_bdev_qos_rate_limit_type type)
+{
+	if (limit->limit == SPDK_BDEV_QOS_LIMIT_NOT_DEFINED) {
+		limit->queue_io = NULL;
+		return;
+	}
+
+	switch (type) {
+	case SPDK_BDEV_QOS_RW_IOPS_RATE_LIMIT:
+		limit->queue_io = bdev_qos_limit_rw_iops_queue;
+		limit->rewind_quota = bdev_qos_limit_rw_iops_rewind_quota;
+		break;
+	case SPDK_BDEV_QOS_RW_BPS_RATE_LIMIT:
+		limit->queue_io = bdev_qos_limit_rw_bps_queue;
+		limit->rewind_quota = bdev_qos_limit_rw_bps_rewind_quota;
+		break;
+	case SPDK_BDEV_QOS_R_BPS_RATE_LIMIT:
+		limit->queue_io = bdev_qos_limit_r_bps_queue;
+		limit->rewind_quota = bdev_qos_limit_r_bps_rewind_quota;
+		break;
+	case SPDK_BDEV_QOS_W_BPS_RATE_LIMIT:
+		limit->queue_io = bdev_qos_limit_w_bps_queue;
+		limit->rewind_quota = bdev_qos_limit_w_bps_rewind_quota;
+		break;
+	default:
+		break;
+	}
+}
+
+static void
 bdev_qos_limits_set_ops(struct bdev_qos_limits *limits)
 {
 	int i;
 
 	for (i = 0; i < SPDK_BDEV_QOS_NUM_RATE_LIMIT_TYPES; i++) {
-		if (limits->rate_limits[i].limit == SPDK_BDEV_QOS_LIMIT_NOT_DEFINED) {
-			limits->rate_limits[i].queue_io = NULL;
-			continue;
-		}
-
-		switch (i) {
-		case SPDK_BDEV_QOS_RW_IOPS_RATE_LIMIT:
-			limits->rate_limits[i].queue_io = bdev_qos_limit_rw_iops_queue;
-			limits->rate_limits[i].rewind_quota = bdev_qos_limit_rw_iops_rewind_quota;
-			break;
-		case SPDK_BDEV_QOS_RW_BPS_RATE_LIMIT:
-			limits->rate_limits[i].queue_io = bdev_qos_limit_rw_bps_queue;
-			limits->rate_limits[i].rewind_quota = bdev_qos_limit_rw_bps_rewind_quota;
-			break;
-		case SPDK_BDEV_QOS_R_BPS_RATE_LIMIT:
-			limits->rate_limits[i].queue_io = bdev_qos_limit_r_bps_queue;
-			limits->rate_limits[i].rewind_quota = bdev_qos_limit_r_bps_rewind_quota;
-			break;
-		case SPDK_BDEV_QOS_W_BPS_RATE_LIMIT:
-			limits->rate_limits[i].queue_io = bdev_qos_limit_w_bps_queue;
-			limits->rate_limits[i].rewind_quota = bdev_qos_limit_w_bps_rewind_quota;
-			break;
-		default:
-			break;
-		}
+		bdev_qos_limit_set_ops(&limits->rate_limits[i], i);
 	}
+}
+
+static void
+bdev_qos_limit_init(struct bdev_qos_limit *limit, enum spdk_bdev_qos_rate_limit_type type)
+{
+	if (bdev_qos_limit_is_iops_rate_limit(type) == true) {
+		limit->min_per_timeslice = SPDK_BDEV_QOS_MIN_IO_PER_TIMESLICE;
+	} else {
+		limit->min_per_timeslice = SPDK_BDEV_QOS_MIN_BYTE_PER_TIMESLICE;
+	}
+
+	limit->limit = SPDK_BDEV_QOS_LIMIT_NOT_DEFINED;
 }
 
 void
 bdev_qos_limits_init(struct bdev_qos_limits *limits)
 {
 	int i;
-	for (i = 0; i < SPDK_BDEV_QOS_NUM_RATE_LIMIT_TYPES; i++) {
-		if (bdev_qos_limit_is_iops_rate_limit(i) == true) {
-			limits->rate_limits[i].min_per_timeslice =
-				SPDK_BDEV_QOS_MIN_IO_PER_TIMESLICE;
-		} else {
-			limits->rate_limits[i].min_per_timeslice =
-				SPDK_BDEV_QOS_MIN_BYTE_PER_TIMESLICE;
-		}
 
-		limits->rate_limits[i].limit = SPDK_BDEV_QOS_LIMIT_NOT_DEFINED;
+	for (i = 0; i < SPDK_BDEV_QOS_NUM_RATE_LIMIT_TYPES; i++) {
+		bdev_qos_limit_init(&limits->rate_limits[i], i);
+	}
+}
+
+static void
+bdev_qos_limit_set(struct bdev_qos_limit *limit, enum spdk_bdev_qos_rate_limit_type type,
+		   uint64_t value)
+{
+	uint32_t limit_set_complement;
+	uint64_t min_limit_per_sec;
+
+	if (bdev_qos_limit_is_iops_rate_limit(type) == true) {
+		limit->limit = value;
+		min_limit_per_sec = SPDK_BDEV_QOS_MIN_IOS_PER_SEC;
+	} else {
+		/* Change from megabyte to byte rate limit */
+		limit->limit = value * 1024 * 1024;
+		min_limit_per_sec = SPDK_BDEV_QOS_MIN_BYTES_PER_SEC;
+	}
+
+	limit_set_complement = limit->limit % min_limit_per_sec;
+	if (limit_set_complement) {
+		SPDK_ERRLOG("Requested rate limit %" PRIu64
+			    " is not a multiple of %" PRIu64 "\n",
+			    value,
+			    min_limit_per_sec);
+		limit->limit += min_limit_per_sec - limit_set_complement;
+		SPDK_ERRLOG("Round up the rate limit to %" PRIu64 "\n", value);
 	}
 }
 
 void
 bdev_qos_limits_set(struct bdev_qos_limits *limits, const uint64_t *values)
 {
-	uint32_t limit_set_complement;
-	uint64_t min_limit_per_sec;
 	int i;
-	uint64_t limit;
 
 	for (i = 0; i < SPDK_BDEV_QOS_NUM_RATE_LIMIT_TYPES; i++) {
-		limit = values[i];
-
-		if (bdev_qos_limit_is_iops_rate_limit(i) == true) {
-			min_limit_per_sec = SPDK_BDEV_QOS_MIN_IOS_PER_SEC;
-		} else {
-			/* Change from megabyte to byte rate limit */
-			limit = limit * 1024 * 1024;
-			min_limit_per_sec = SPDK_BDEV_QOS_MIN_BYTES_PER_SEC;
-		}
-
-		limit_set_complement = limit % min_limit_per_sec;
-		if (limit_set_complement) {
-			SPDK_ERRLOG("Requested rate limit %" PRIu64
-				    " is not a multiple of %" PRIu64 "\n",
-				    values[i],
-				    min_limit_per_sec);
-			limit += min_limit_per_sec - limit_set_complement;
-			SPDK_ERRLOG("Round up the rate limit to %" PRIu64 "\n", values[i]);
-		}
-
-		limits->rate_limits[i].limit = limit;
+		bdev_qos_limit_set(&limits->rate_limits[i], i, values[i]);
 	}
+}
+
+static void
+bdev_qos_limit_update_max_quota_per_timeslice(struct bdev_qos_limit *limit,
+		enum spdk_bdev_qos_rate_limit_type type)
+{
+	uint64_t max_per_timeslice;
+
+	if (bdev_qos_limit_is_iops_rate_limit(type) == true) {
+		limit->min_per_timeslice = SPDK_BDEV_QOS_MIN_IO_PER_TIMESLICE;
+	} else {
+		limit->min_per_timeslice = SPDK_BDEV_QOS_MIN_BYTE_PER_TIMESLICE;
+	}
+
+	if (limit->limit == SPDK_BDEV_QOS_LIMIT_NOT_DEFINED) {
+		limit->max_per_timeslice = 0;
+		return;
+	}
+
+	max_per_timeslice = limit->limit * SPDK_BDEV_QOS_TIMESLICE_IN_USEC / SPDK_SEC_TO_USEC;
+
+	limit->max_per_timeslice = spdk_max(max_per_timeslice, limit->min_per_timeslice);
+
+	__atomic_store_n(&limit->remaining_this_timeslice, limit->max_per_timeslice,
+			 __ATOMIC_RELEASE);
 }
 
 void
 bdev_qos_limits_update_max_quota_per_timeslice(struct bdev_qos_limits *limits)
 {
-	uint64_t max_per_timeslice;
 	int i;
 
 	for (i = 0; i < SPDK_BDEV_QOS_NUM_RATE_LIMIT_TYPES; i++) {
-		if (bdev_qos_limit_is_iops_rate_limit(i) == true) {
-			limits->rate_limits[i].min_per_timeslice =
-				SPDK_BDEV_QOS_MIN_IO_PER_TIMESLICE;
-		} else {
-			limits->rate_limits[i].min_per_timeslice =
-				SPDK_BDEV_QOS_MIN_BYTE_PER_TIMESLICE;
-		}
-
-		if (limits->rate_limits[i].limit == SPDK_BDEV_QOS_LIMIT_NOT_DEFINED) {
-			limits->rate_limits[i].max_per_timeslice = 0;
-			continue;
-		}
-
-		max_per_timeslice = limits->rate_limits[i].limit *
-				    SPDK_BDEV_QOS_TIMESLICE_IN_USEC / SPDK_SEC_TO_USEC;
-
-		limits->rate_limits[i].max_per_timeslice = spdk_max(max_per_timeslice,
-				limits->rate_limits[i].min_per_timeslice);
-
-		__atomic_store_n(&limits->rate_limits[i].remaining_this_timeslice,
-				 limits->rate_limits[i].max_per_timeslice, __ATOMIC_RELEASE);
+		bdev_qos_limit_update_max_quota_per_timeslice(&limits->rate_limits[i], i);
 	}
 
 	bdev_qos_limits_set_ops(limits);
+}
+
+static inline void
+bdev_qos_limit_rewind(struct bdev_qos_limit *limit, struct spdk_bdev_io *bdev_io)
+{
+	if (!limit->queue_io) {
+		return;
+	}
+
+	limit->rewind_quota(limit, bdev_io);
 }
 
 void
@@ -313,12 +342,18 @@ bdev_qos_limits_rewind(struct bdev_qos_limits *limits, struct spdk_bdev_io *bdev
 	int i;
 
 	for (i = 0; i < SPDK_BDEV_QOS_NUM_RATE_LIMIT_TYPES; i++) {
-		if (!limits->rate_limits[i].queue_io) {
-			continue;
-		}
-
-		limits->rate_limits[i].rewind_quota(&limits->rate_limits[i], bdev_io);
+		bdev_qos_limit_rewind(&limits->rate_limits[i], bdev_io);
 	}
+}
+
+static inline bool
+bdev_qos_limit_queue_io(struct bdev_qos_limit *limit, struct spdk_bdev_io *bdev_io)
+{
+	if (!limit->queue_io) {
+		return false;
+	}
+
+	return limit->queue_io(limit, bdev_io);
 }
 
 bool
@@ -327,17 +362,9 @@ bdev_qos_limits_queue_io(struct bdev_qos_limits *limits, struct spdk_bdev_io *bd
 	int i;
 
 	for (i = 0; i < SPDK_BDEV_QOS_NUM_RATE_LIMIT_TYPES; i++) {
-		if (!limits->rate_limits[i].queue_io) {
-			continue;
-		}
-
-		if (limits->rate_limits[i].queue_io(&limits->rate_limits[i], bdev_io) == true) {
+		if (bdev_qos_limit_queue_io(&limits->rate_limits[i], bdev_io) == true) {
 			for (i -= 1; i >= 0 ; i--) {
-				if (!limits->rate_limits[i].queue_io) {
-					continue;
-				}
-
-				limits->rate_limits[i].rewind_quota(&limits->rate_limits[i], bdev_io);
+				bdev_qos_limit_rewind(&limits->rate_limits[i], bdev_io);
 			}
 			return true;
 		}
@@ -346,39 +373,50 @@ bdev_qos_limits_queue_io(struct bdev_qos_limits *limits, struct spdk_bdev_io *bd
 	return false;
 }
 
+static inline void
+bdev_qos_limit_reset_quota(struct bdev_qos_limit *limit, int timeslice_count)
+{
+	int64_t remaining_last_timeslice;
+
+	/* We may have allowed the IOs or bytes to slightly overrun in the last
+	 * timeslice. remaining_this_timeslice is signed, so if it's negative
+	 * here, we'll account for the overrun so that the next timeslice will
+	 * be appropriately reduced.
+	 */
+	remaining_last_timeslice = __atomic_exchange_n(&limit->remaining_this_timeslice,
+				   0, __ATOMIC_RELAXED);
+	if (remaining_last_timeslice < 0) {
+		/* There could be a race condition here as both bdev_qos_rw_queue_io() and bdev_channel_poll_qos()
+		 * potentially use 2 atomic ops each, so they can intertwine.
+		 * This race can potentialy cause the limits to be a little fuzzy but won't cause any real damage.
+		 */
+		__atomic_store_n(&limit->remaining_this_timeslice, remaining_last_timeslice,
+				 __ATOMIC_RELAXED);
+	}
+
+	if (timeslice_count > 0) {
+		__atomic_add_fetch(&limit->remaining_this_timeslice,
+				   limit->max_per_timeslice * timeslice_count,
+				   __ATOMIC_RELAXED);
+	}
+}
+
 void
 bdev_qos_limits_reset_quota(struct bdev_qos_limits *limits,
 			    uint64_t now,
 			    uint64_t timeslice_size,
 			    uint64_t *last_timeslice)
 {
+	int timeslice_count = 0;
 	int i;
-	int64_t remaining_last_timeslice;
-
-	for (i = 0; i < SPDK_BDEV_QOS_NUM_RATE_LIMIT_TYPES; i++) {
-		/* We may have allowed the IOs or bytes to slightly overrun in the last
-		 * timeslice. remaining_this_timeslice is signed, so if it's negative
-		 * here, we'll account for the overrun so that the next timeslice will
-		 * be appropriately reduced.
-		 */
-		remaining_last_timeslice = __atomic_exchange_n(&limits->rate_limits[i].remaining_this_timeslice,
-					   0, __ATOMIC_RELAXED);
-		if (remaining_last_timeslice < 0) {
-			/* There could be a race condition here as both bdev_qos_rw_queue_io() and bdev_channel_poll_qos()
-			 * potentially use 2 atomic ops each, so they can intertwine.
-			 * This race can potentialy cause the limits to be a little fuzzy but won't cause any real damage.
-			 */
-			__atomic_store_n(&limits->rate_limits[i].remaining_this_timeslice,
-					 remaining_last_timeslice, __ATOMIC_RELAXED);
-		}
-	}
 
 	while (now >= (*last_timeslice + timeslice_size)) {
 		*last_timeslice += timeslice_size;
-		for (i = 0; i < SPDK_BDEV_QOS_NUM_RATE_LIMIT_TYPES; i++) {
-			__atomic_add_fetch(&limits->rate_limits[i].remaining_this_timeslice,
-					   limits->rate_limits[i].max_per_timeslice, __ATOMIC_RELAXED);
-		}
+		timeslice_count++;
+	}
+
+	for (i = 0; i < SPDK_BDEV_QOS_NUM_RATE_LIMIT_TYPES; i++) {
+		bdev_qos_limit_reset_quota(&limits->rate_limits[i], timeslice_count);
 	}
 }
 
