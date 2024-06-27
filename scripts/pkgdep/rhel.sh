5451a262e15cbcacae3a3444f0f7663684a3e0f9
Evgeniy Kochetov <evgeniik@nvidia.com>
2024-06-27 19:04:20 +0300

    scripts/pkgdep: Fix Oracle Linux support
    Signed-off-by: Evgeniy Kochetov <evgeniik@nvidia.com>

--- scripts/pkgdep/rhel.sh
+++ scripts/pkgdep/rhel.sh
@@ -121,7 +121,7 @@ fi
 if echo "$ID $VERSION_ID" | grep -E -q 'centos 7|rhel 7'; then
 	yum install -y openssl11-devel
 fi
-if echo "$ID $VERSION_ID" | grep -E -q 'centos 8|rhel 8|rocky 8'; then
+if echo "$ID $VERSION_ID" | grep -E -q 'centos 8|rhel 8|rocky 8|ol 8'; then
 	yum install -y python36 python36-devel
 	#Create hard link to use in SPDK as python
 	if [[ ! -e /usr/bin/python && -e /etc/alternatives/python3 ]]; then
